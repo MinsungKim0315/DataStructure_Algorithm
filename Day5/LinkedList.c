add0f5a2422c5419f056347614ce535cfa147d8b
Minsung Kim <88697052+MinsungKim0315@users.noreply.github.com>
2024-04-23 16:47:32 +0900

    Update LinkedList.c

--- Day5/LinkedList.c
+++ Day5/LinkedList.c
@@ -51,12 +51,71 @@ ListNode* reversed(ListNode* head) {	//연결 방향 바꾸기
 	return pNode;
 }
 
-ListNode* insert_last(ListNode* head, list_element value);// 맨 뒤 노드 생성
-ListNode* insert_sort(ListNode* head, list_element value);// 정렬된 노드 생성
-ListNode* delete_last(ListNode* head);// 맨 뒤 노드 삭제
-int delete_search(ListNode** head, list_element value);// 검색된 노드 삭제
-ListNode* ListSort(ListNode* head);// 노드 정렬
-void freelist(ListNode** head);// 리스트 메모리 해제
+
+ListNode* insert_last(ListNode* head, list_element value) {//맨 뒤 노드 생성
+	ListNode* newNode = (ListNode*)malloc(sizeof(ListNode));
+	newNode->data = value;
+	newNode->link = NULL;
+
+	if (head == NULL) return newNode;
+	ListNode* pNode = head;
+	while (pNode->link != NULL) {	//traverse to the last node
+		pNode = pNode->link;
+	}
+	pNode->link = newNode;
+	return head;
+}
+
+ListNode* delete_last(ListNode* head){	//맨 뒤 노드 삭제
+	if (head == NULL) return NULL;
+	ListNode* pNode = head;
+	ListNode* prevNode = NULL;
+
+	while (pNode->link != NULL) {
+		prevNode = pNode;
+		pNode = pNode->link;
+	}
+	free(pNode);
+	prevNode->link = NULL;
+
+	return head;
+}
+
+int delete_search(ListNode* head, list_element value) {	// 검색된 노드 삭제
+	if (head == NULL) return NULL;
+
+	ListNode* pNode = head;
+	ListNode* prevNode = NULL;
+
+	if (pNode->data == value) {
+		ListNode* newHead = pNode->link;
+		free(pNode);
+		return newHead;
+	}
+
+	while (pNode != NULL && pNode->data != value) {
+		prevNode = pNode;
+		pNode = pNode->link;	//keep track of the previous node
+	}
+
+	if (pNode == NULL) return head;	//if the node was not found
+
+	if (prevNode != NULL) {	//prevNode가 NULL인 경우는 첫 번째 노드를 제거하는 경우임
+		prevNode->link = pNode->link;
+	}
+	free(pNode);	//찾은 노드 제거
+	return head;
+}
+
+void freelist(ListNode** head) {	// 리스트 메모리 해제
+	ListNode* DeleteNode = NULL;
+	while (*head) {
+		DeleteNode = *head;
+		*head = (*head)->link;
+		free(DeleteNode);	//Free the memory allocated for the node pointed to by DeleteNode
+	}
+	*head = NULL;	//헤드를 NULL로 초기화
+}
 
 void print_list(ListNode* head) {
 	ListNode* pNode = head;
@@ -72,29 +131,6 @@ int main() {
 	ListNode* head1 = NULL;
 	ListNode* head2 = NULL;
 
-	/*ListNode n1;
-	n1.data = 10;
-	n1.link = NULL;
-
-	head1 = &n1;
-	
-	ListNode n2;
-	n2.data = 5;
-	n2.link = &n1;
-
-	head1 = &n2;
-
-	ListNode n3;
-	n3.data = 2;
-	n3.link = &n2;
-
-	head1 = &n3;
-
-	ListNode n4;
-	n4.data = 1;
-	n4.link = &n3;
-
-	head1 = &n4;*/
 	//맨 앞에 노드 추가, 연결 리스트 생성
 	head1 = insertHead(head1, 10);
 	head1 = insertHead(head1, 5);
@@ -117,10 +153,23 @@ int main() {
 	else
 		printf("unfound");*/
 
+	//concat
+	// concat(head1, head2);
+	
+	//끝에 노드 추가
+	insert_last(head1, 100);
+
+	//끝 노드 제거
+	delete_last(head1);
+
+	//검색한 노드 삭제
+	delete_search(head2, 10);
+
+	//리스트 전체 메모리서 제거
+	//freelist(&head2);
+
 	//출력
 	print_list(head1);
 	print_list(head2);
-	concat(head1, head2);
-	head1 = reversed(head1);
-	print_list(head1);
+	
 }
