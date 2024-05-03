d4f1b0636c09c05e58990759be33c0f290923953
Minsung Kim <88697052+MinsungKim0315@users.noreply.github.com>
2024-05-03 14:32:43 +0900

    Update Bucket_Sort_queue.c

--- Day14/Bucket_Sort_queue.c
+++ Day14/Bucket_Sort_queue.c
@@ -1,13 +1,14 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<time.h>
+#define swap(type, x, y){type t = x; x = y;y = t;}
 
 typedef int QueueElement;
 typedef struct QueueNode {
 	QueueElement data;
 	struct QueueNode* link;
 }QueueNode;
-
-typedef struct Queue{
+typedef struct Queue {
 	QueueNode* front, * rear;
 }LinkedQueueType;
 
@@ -18,11 +19,9 @@ LinkedQueueType* QueueInit() {
 
 	return newQueue;
 }
-
 int QueueIsEmpty(LinkedQueueType* queue) {
 	return (queue->front == NULL);
 }
-
 void Enqueue(LinkedQueueType* queue, QueueElement data) {
 	QueueNode* newNode = (QueueNode*)malloc(sizeof(QueueNode));
 	if (newNode == NULL) exit(1);
@@ -37,7 +36,6 @@ void Enqueue(LinkedQueueType* queue, QueueElement data) {
 		queue->rear = newNode;
 	}
 }
-
 QueueElement Dequeue(LinkedQueueType* queue) {
 	if (QueueIsEmpty(queue)) return 0;
 	QueueNode* deleteNode = queue->front;
@@ -48,14 +46,6 @@ QueueElement Dequeue(LinkedQueueType* queue) {
 
 	return data;
 }
-
-void PrintQueue(LinkedQueueType* queue) {
-	printf("queue: ");
-	for (QueueNode* pNode = queue->front; pNode != NULL; pNode = pNode->link) {
-		printf("%4d --> ", pNode->data);
-	}printf("\n");
-}
-
 void FreeQueue(LinkedQueueType* queue) {
 	for (QueueNode* pNode = queue->front; pNode != NULL;) {
 		QueueNode* deleteNode = pNode;
@@ -65,71 +55,43 @@ void FreeQueue(LinkedQueueType* queue) {
 	free(queue);
 }
 
-void Endeque(LinkedQueueType* queue, QueueElement data, int direction) {
-	QueueNode* newNode = (QueueNode*)malloc(sizeof(QueueNode));
-	if (newNode == NULL) exit(1);
-	newNode->data = data;
-	newNode->link = NULL;
-	if (QueueIsEmpty(queue)) {
-		queue->front = newNode;
-		queue->rear = newNode;
-	}
-	else if (direction == 1) {
-		newNode->link = queue->front;
-		queue->front = newNode;
-	}
-	else {
-		queue->rear->link = newNode;
-		queue->rear = newNode;
-	}
-
+void display(int* arr, int size) {
+	for (int i = 0; i < size; i++) {
+		printf("%7d ", arr[i]);
+	}printf("\n");
 }
+void randomize(int* arr, int str, int end, int size) {
+	if (str > end) swap(int, str, end);
+	srand((unsigned int)time(NULL));
+	for (int i = 0; i < size; i++) arr[i] = rand() % (end - str + 1) + str;
+}
+void bucket(int* arr, int size) {
+	LinkedQueueType* bucket1[10];
+	LinkedQueueType* bucket2[10];
+	for (int i = 0; i < 10; i++) {
+		bucket1[i] = QueueInit();
+		bucket2[i] = QueueInit();
+	}
 
-QueueElement Dedeque(LinkedQueueType* queue, int direction) {
-	if (QueueIsEmpty(queue)) return 0;
-	QueueNode* deleteNode = NULL;
-	QueueElement data = NULL;
-	if (queue->front->link == NULL) queue->front = queue->rear = NULL;
-	if (direction == 1) {
-		deleteNode = queue->rear;
-		data = deleteNode->data;
-		QueueNode* pNode = NULL;
-		for (pNode = queue->front; pNode->link->link != NULL; ) {
-			pNode = pNode->link;
+	for (int i = 0; i < size; i++) {
+		Enqueue(bucket1[arr[i] % 10], arr[i]);
+	}
+
+	for (int i = 0; i < 10; i++) {
+		while (QueueIsEmpty(bucket1[i]) != 1) {
+			int temp = Dequeue(bucket1[i]);
+			Enqueue(bucket1[temp / 10], temp);
 		}
-		pNode->link = NULL;
-		queue->rear = pNode;
 	}
-	else {
-		deleteNode = queue->front;
-		data = deleteNode->data;
-		queue->front = deleteNode->link;
+	for (int i = 0, j= 0; i < 10; i++) {
+		while (QueueIsEmpty(bucket2[i]) != 1) {
+			int temp = Dequeue(bucket2);
+			arr[j++] = temp;
+		}
+	}
+	for (int i = 0; i < 10; i++) {
+		FreeQueue(bucket1[i]);
+		FreeQueue(bucket2[i]);
 	}
 
-	free(deleteNode);
-
-	return data;
-}
-
-int main() {
-	LinkedQueueType* queue1 = QueueInit();
-	Enqueue(queue1, 6);
-	Enqueue(queue1, 1);
-	Enqueue(queue1, 2);
-	Enqueue(queue1, 3);
-	Enqueue(queue1, 4);
-	Enqueue(queue1, 5);
-	Enqueue(queue1, 7);
-	PrintQueue(queue1);
-
-	Dequeue(queue1);
-	PrintQueue(queue1);
-
-	Endeque(queue1, 0, 1);
-	PrintQueue(queue1);
-
-	Dedeque(queue1, 1);
-	PrintQueue(queue1);
-
-	FreeQueue(queue1);
 }
