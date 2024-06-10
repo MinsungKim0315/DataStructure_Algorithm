8ae6c218d5799a2a14704071bcaf2369c4b6a4da
Minsung Kim <88697052+MinsungKim0315@users.noreply.github.com>
2024-06-10 14:47:55 +0900

    Update IndexedSequential_Search.c

--- Day15/IndexedSequential_Search.c
+++ Day15/IndexedSequential_Search.c
@@ -1,14 +1,43 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+int seq_search(int* arr, int low, int high, int key) {
+	for (int i = low; i <= high; i++) if (arr[i] == key) return i;
+	return -1;
+}
 #define INDEX_SIZE 5
-
 typedef struct itable {
 	int index;
 	int key;
 }itable;
 itable indexTable[INDEX_SIZE];
-
+void makeIndexTable(int MainTable[], int size) {
+	int gap = size / INDEX_SIZE;
+	for (int i = 0; i < INDEX_SIZE; i++) {
+		indexTable[i].index = i * gap;
+		indexTable[i].key = MainTable[i * gap];
+	}
+}
+int indexSearch(int MainTable[], int low, int high, int key) {
+	int begin = low, end = high;
+	if (key < MainTable[begin] || key > MainTable[end]) return -1;
+	for (int i = 1; i < INDEX_SIZE; i++) {
+		if ((indexTable[i - 1].key <= key) && (indexTable[i].key > key)) {	//find the range between two index points 
+			begin = indexTable[i - 1].index;
+			end = indexTable[i].index;
+			break;
+		}
+		else {
+			begin = indexTable[i].index;
+			end = high;
+		}
+	}
+	return seq_search(MainTable, begin, end, key);
+}
+void display(int* arr, int size) {
+	for (int i = 0; i < size; i++) printf("%3d ", arr[i]);
+	printf("\n");
+}
 void quick(int* arr, int st, int end) {
 	int left = st, right = end, pivot = arr[(st + end) / 2];
 	while (left <= right) {
@@ -22,57 +51,17 @@ void quick(int* arr, int st, int end) {
 	if (st < right) quick(arr, st, right);
 	if (left < end)  quick(arr, left, end);
 }
-void display(int* arr, int size) {
-	for (int i = 0; i < size; i++) printf("%3d ", arr[i]);
-	printf("\n");
-}
-int linear_search(int* arr, int key, int low, int high) {
-	for (int i = low; i <= high; i++) {
-		if (arr[i] == key) return i;
-	}
-	return -1;
-}
-
-void makeIndexTable(int MainTable[], int size) {
-	int gap = size / INDEX_SIZE;
-	for (int i = 0; i < INDEX_SIZE; i++) {
-		indexTable[i].index = i * gap;
-		indexTable[i].key = MainTable[i * gap];
-	}
-}
-int indexSearch(int MainTable[], int key, int low, int high) {
-	int begin = low, end = high;
-	if (key < MainTable[begin] || key > MainTable[end]) return -1;
-	for (int i = 1; i < INDEX_SIZE; i++) {
-		if (indexTable[i - 1].key <= key && indexTable[i].key > key) {
-			begin = indexTable[i - 1].index;
-			end = indexTable[i].index;
-			break;
-		}
-		else {
-			begin = indexTable[i].index;
-			end = high;
-		}
-	}
-	return linear_search(MainTable, key, begin, end);
-}
 int main() {
 	srand(time(NULL));
 	int n = 20;
 	int range = 20;
-	int key;
 	int* a = (int*)calloc(n, sizeof(int));
-	printf("\n\t<< »öÀÎ ¼øÂ÷ °Ë»ö >>\n");
+	printf("\n\t<< ìƒ‰ì¸ ìˆœì°¨ ê²€ìƒ‰ >>\n");
 	for (int i = 0; i < n; i++) a[i] = rand() % range;
 	quick(a, 0, n - 1);
 	display(a, n);
-
 	makeIndexTable(a, n);
-	while (1)
-	{
-		printf("Select a number to find(0~20): ");
-		scanf_s("%d", &key);
-		printf("%d\n", indexSearch(a, key, 0, n-1));
-	}
+	for (int i = 0; i < range; i++)
+		printf(" %3d ì˜ ìœ„ì¹˜ëŠ” a[%3d] ì¹¸ \n", i, indexSearch(a, 0, n - 1, i));
 	return 0;
 }
