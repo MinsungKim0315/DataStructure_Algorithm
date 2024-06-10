d1a6c3bae4e0774e168c9704965ac39b039d1fbe
Minsung Kim <88697052+MinsungKim0315@users.noreply.github.com>
2024-06-10 14:34:32 +0900

    Update Linear_Search.c

--- Day15/Linear_Search.c
+++ Day15/Linear_Search.c
@@ -2,19 +2,6 @@
 #include<stdlib.h>
 #include<time.h>
 
-void quick(int* arr, int st, int end) {
-	int left = st, right = end, pivot = arr[(st + end) / 2];
-	while (left <= right) {
-		while (arr[left] < pivot)  left++;
-		while (pivot < arr[right])  right--;
-		if (left <= right) {
-			int temp = arr[left]; arr[left] = arr[right]; arr[right] = temp;
-			left++, right--;
-		}
-	}
-	if (st < right) quick(arr, st, right);
-	if (left < end)  quick(arr, left, end);
-}
 void display(int* arr, int size) {
 	for (int i = 0; i < size; i++) printf("%3d ", arr[i]);
 	printf("\n");
@@ -34,7 +21,6 @@ int main() {
 	int* a = (int*)calloc(n, sizeof(int));
 	printf("\n\t<< Linear Search >>\n");
 	for (int i = 0; i < n; i++) a[i] = rand() % range;
-	quick(a, 0, n - 1);
 	display(a, n);
 	printf("Select a number to find(0~20): ");
 	scanf_s("%d", &key);
