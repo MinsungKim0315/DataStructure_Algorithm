ca799effb55a6b2a3046c9b4d9fd39e6c6f736cf
Minsung Kim <88697052+MinsungKim0315@users.noreply.github.com>
2024-06-17 10:59:49 +0900

    Update Tree.c

--- Day17/Tree.c
+++ Day17/Tree.c
@@ -3,62 +3,64 @@
 
 typedef struct treeNode {
 	int value;
-	struct treeNode* left;
 	struct treeNode* right;
+	struct treeNode* left;
 }treeNode;
+
+treeNode* setBinTreeNode(int* arr, int count, int idx);
+void displayPreOrder(treeNode* t);
+void displayInOrder(treeNode* t);
+void displayPostOrder(treeNode* t);
+void freeTree(treeNode* t);
 
 treeNode* setBinTreeNode(int* arr, int count, int idx) {
 	treeNode* node = (treeNode*)malloc(sizeof(treeNode));
 	node->value = arr[idx];
 	node->left = node->right = NULL;
 	int LEFT = idx * 2 + 1, RIGHT = LEFT + 1;
-	if (LEFT < count) {
-		node->left = setBinTreeNode(arr, count, LEFT);
-	}
-	if (RIGHT < count) {
-		node->right = setBinTreeNode(arr, count, RIGHT);
-	}
+	if (LEFT < count) node->left = setBinTreeNode(arr, count, LEFT);
+	if (RIGHT < count) node->right = setBinTreeNode(arr, count, RIGHT);
 
 	return node;
 }
-void displayPreOrder(treeNode* t) {
+void displayPreOrder(treeNode* t) {	//parent -> left -> right
 	if (t != NULL) {
-		printf("%3d->", t->value);	//parent
+		printf("%3d", t->value);
 		displayPreOrder(t->left);
 		displayPreOrder(t->right);
 	}
 }
-void displayInOrder(treeNode* t) {
+void displayInOrder(treeNode* t) {	//left -> parent -> right
 	if (t != NULL) {
 		displayInOrder(t->left);
-		printf("%3d->", t->value);
+		printf("%3d", t->value);
 		displayInOrder(t->right);
 	}
 }
-void displayPostOrder(treeNode* t) {
+void displayPostOrder(treeNode* t) {	//left -> right -> parent
 	if (t != NULL) {
 		displayPostOrder(t->left);
 		displayPostOrder(t->right);
-		printf("%3d->", t->value);
+		printf("%3d", t->value);
 	}
 }
 void freeTree(treeNode* t) {
 	if (t != NULL) {
 		freeTree(t->left);
 		freeTree(t->right);
-		freeTree(t);
+		free(t);
 	}
 }
 int main() {
 	int arr[] = { 8 , 4 , 12 , 2 , 6 , 10 , 14 , 1 , 3 , 5 , 7 , 9 , 11 , 13 , 15 };
 	treeNode* root = setBinTreeNode(arr, sizeof(arr) / sizeof(arr[0]), 0);
-	printf("\n전위 순회\n");
+
+	printf("Pre-Order\n");
 	displayPreOrder(root);
-	printf("\n중위 순회\n");
+	printf("\nIn-Order\n");
 	displayInOrder(root);
-	printf("\n후위 순회\n");
+	printf("\nPost-Order\n");
 	displayPostOrder(root);
-	printf("\n노드 삭제\n");
+
 	freeTree(root);
-	return 0;
 }
