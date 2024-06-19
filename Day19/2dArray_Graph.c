dffd709b6440d356775e3afecbb583c2e5a68dee
Minsung Kim <88697052+MinsungKim0315@users.noreply.github.com>
2024-06-19 12:02:30 +0900

    Update 2dArray_Graph.c

--- Day19/2dArray_Graph.c
+++ Day19/2dArray_Graph.c
@@ -1,65 +1,54 @@
-//2차원 배열로 표현한 그래프
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <math.h>
-///// Weighted Graph /////
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#include<math.h>
+
 typedef struct graph {
-	int verCount; //정점의 개수
-	int** arr; //동적 할당 된 2차원 배열의 주소 저장
+	int verCount;	//num of vertex
+	int** arr;		//address of allocated 2d array
 }graph;
+
+// ADT
 graph* graphInitialize(int count);
-void addDirectedEdge(graph* p, int src, int dest, int weight);
-void addUnDirectedEdge(graph* p, int src, int dest, int weight);
+void addDirectedEdge(graph* p, int src, int dest);
+void addUnDirectedEdge(graph* p, int src, int dest);
 void graphDisplay(graph* p);
 void memoryFree(graph* p);
+
 int main() {
-	graph* grp1 = graphInitialize(4); //구조체, 정점의 개수
-	graph* grp2 = graphInitialize(4); 
-
-	addUnDirectedEdge(grp1, 0, 1, 2); //구조체, 출발지, 도착지, 가중치
-	addUnDirectedEdge(grp1, 0, 2, 3);
-	addUnDirectedEdge(grp1, 1, 2, 4);
-	addUnDirectedEdge(grp1, 2, 3, 5);
-
-	addDirectedEdge(grp2, 0, 1, 2);
-	addDirectedEdge(grp2, 0, 2, 3);
-	addDirectedEdge(grp2, 1, 2, 4);
-	addDirectedEdge(grp2, 2, 3, 5);
-
-	graphDisplay(grp1);
-	graphDisplay(grp2);
-
-	memoryFree(grp1);
-	memoryFree(grp2);
+	graph* grp = graphInitialize(4); 
+	addUnDirectedEdge(grp, 0, 1);
+	addUnDirectedEdge(grp, 0, 2);
+	addUnDirectedEdge(grp, 1, 2);
+	addUnDirectedEdge(grp, 2, 3);
+	graphDisplay(grp);
+	memoryFree(grp);
 	return 0;
 }
 
 graph* graphInitialize(int count) {
 	graph* pG = (graph*)malloc(sizeof(graph));
 	pG->verCount = count;
-	pG->arr = (int**)calloc(count, sizeof(int*));
-	for (int i = 0; i < count; i++) {
+	pG->arr = (int**)calloc(count, sizeof(int*));	//create rows
+	for (int i = 0; i < count; i++) {	//create columns
 		pG->arr[i] = (int*)calloc(count, sizeof(int));
 	}
-
 	return pG;
 }
-
-void addDirectedEdge(graph* p, int src, int dest, int weight) {  //방향 그래프
-	p->arr[src][dest] = weight;
+void addDirectedEdge(graph* p, int src, int dest) {
+	p->arr[src][dest] = 1;
 }
-void addUnDirectedEdge(graph* p, int src, int dest, int weight) { //무방향 그래프
-	addDirectedEdge(p, src, dest, weight);
-	addDirectedEdge(p, dest, src, weight);
+void addUnDirectedEdge(graph* p, int src, int dest) {
+	addDirectedEdge(p, src, dest);
+	addDirectedEdge(p, dest, src);
 }
 void graphDisplay(graph* p) {
-	printf("\n\n *** Graph 인접 행렬 ***\n\n    │");
+	printf("\n\n *** Graph in 2d-array ***\n\n    │");
 	for (int i = 0; i < p->verCount; i++) printf(" %3d", i);
 	printf("\n────┼");
 	for (int i = 0; i < p->verCount; i++) printf("────");
 	printf("\n");
-	for (int i = 0; i < p->verCount; i++) {  //vertex의 개수만큼 반복
+	for (int i = 0; i < p->verCount; i++) {
 		printf("%3d │ ", i);
 		for (int j = 0; j < p->verCount; j++) {
 			printf("%3d ", p->arr[i][j]);
@@ -68,7 +57,9 @@ void graphDisplay(graph* p) {
 	}
 }
 void memoryFree(graph* p) {
-	for (int i = 0; i < p->verCount; i++) free(p->arr[i]); //free(포인터); 포인터가 가리키는 메모리가 해제
+	for (int i = 0; i < p->verCount; i++) {
+		free(p->arr[i]);
+	}
 	free(p->arr);
 	free(p);
 }
