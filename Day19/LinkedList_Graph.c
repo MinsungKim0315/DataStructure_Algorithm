4f53edf63fda2335c9b4c18ec8128b47eb908b0b
Minsung Kim <88697052+MinsungKim0315@users.noreply.github.com>
2024-06-19 12:03:04 +0900

    Update LinkedList_Graph.c

--- Day19/LinkedList_Graph.c
+++ Day19/LinkedList_Graph.c
@@ -1,35 +1,39 @@
-// 연결 리스트 그래프, Kruskal MST
-#include <stdio.h>
-#include <stdlib.h>
-///// Linked List Graph /////
+#include<stdio.h>
+#include<stdlib.h>
+
 typedef struct node {
-	int dst;      //도착지
-	struct node* next;  //다음 노드의 주소 저장
-} node;
-typedef struct graph {//간선을 저장하기 위한 그래프
-	int count;  //정점의 개수
-	node* arr;  //정점의 개수만큼 할당 된 node(출발지)
-} graph;
+	int dst;
+	struct node* next;
+}node;
+typedef struct graph {
+	int count;
+	node* arr;
+}graph;
+
+//ADT
 graph* graphInitialize(int count);
 void addDirectedEdge(graph* grp, int src, int dst);
 void addUnDirectedEdge(graph* grp, int src, int dst);
 void graphDisplay(graph* grp);
 void memoryFree(graph* grp);
+
 int main() {
-	graph* grp = graphInitialize(4); //그래프, 정점의 개수
+	graph* grp = graphInitialize(4);
 	addUnDirectedEdge(grp, 0, 1);
 	addUnDirectedEdge(grp, 0, 2);
 	addUnDirectedEdge(grp, 1, 2);
 	addUnDirectedEdge(grp, 2, 3);
 	graphDisplay(grp);
 	memoryFree(grp);
+
 	return 0;
 }
-///// Linked List Graph /////
+
 graph* graphInitialize(int count) {
 	graph* grp = (graph*)malloc(sizeof(graph));
 	grp->count = count;
 	grp->arr = (node*)calloc(count, sizeof(node));
+
 	return grp;
 }
 void addDirectedEdge(graph* grp, int src, int dst) {
@@ -42,23 +46,24 @@ void addUnDirectedEdge(graph* grp, int src, int dst) {
 	addDirectedEdge(grp, src, dst);
 	addDirectedEdge(grp, dst, src);
 }
-void graphDisplay(graph* grp) { // 출발 정점 -> 간선 연결 표 출력
+void graphDisplay(graph* grp) {
 	if (grp == NULL) return;
-	for (int i = 0; i < grp->count; i++) { //정점의 개수만큼 반복
-		printf("출발지 노드 : %d ==> ", i); // 출발지 정점 출력
-		for (node* cur = grp->arr[i].next; cur; cur = cur->next)
-			printf("%d ", cur->dst); // 인접 정점 출력
+	for (int i = 0; i < grp->count; i++) {
+		printf("Start Node: %d ==> ", i);
+		for (node* cur = grp->arr[i].next; cur; cur = cur->next) {
+			printf("%d ", cur->dst);
+		}
 		printf("\n");
 	}
 }
 void memoryFree(graph* grp) {
 	for (int i = 0; i < grp->count; i++) {
-		while (grp->arr[i].next) { // 인접 정점(노드) 삭제
+		while (grp->arr[i].next) { 
 			node* delNode = grp->arr[i].next;
-			grp->arr[i].next = delNode->next; // 다음 노드 주소로 변경
+			grp->arr[i].next = delNode->next; 
 			free(delNode);
 		}
 	}
-	free(grp->arr); // 출발지 노드 삭제
+	free(grp->arr);
 	free(grp);
 }
